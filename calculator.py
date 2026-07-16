1ce7f006f7cfb7a51b79b5d98bbc88619892baef
krithikashree1957 <krithikashr@gmail.com>
2026-07-16 11:15:02 +0530

    version2

--- calculator.py
+++ calculator.py
@@ -4,8 +4,16 @@ def add(a, b):
  return a + b
 def subtract(a, b):
  return a - b
+def multiply(a, b):
+ return a * b
+def divide(a, b):
+ if b == 0:
+     return "Cannot divide by zero"
+ return a/b
 if __name__ == "__main__":
  x = 10
  y = 5
- print("Sum:", add(x, y))
- print("Difference:", subtract(x, y))
+print("Sum:", add(x, y))
+print("Difference:", subtract(x, y))
+print("multiply:", multiply(x, y))
+print("Divide:", divide(x, y))
